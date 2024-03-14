273af4e3f838dc674641f1d5f4dcda4c56985130
pyodol2 <145510412+pyodol2@users.noreply.github.com>
2024-03-14 13:52:06 +0900

    commit

---
+++ delivery/src/main/java/labshoppubsub/domain/DeliveryAdded.java
@@ -0,0 +1,27 @@
+package labshoppubsub.domain;
+
+import java.time.LocalDate;
+import java.util.*;
+import labshoppubsub.domain.*;
+import labshoppubsub.infra.AbstractEvent;
+import lombok.*;
+
+//<<< DDD / Domain Event
+@Data
+@ToString
+public class DeliveryAdded extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private Long productId;
+    private Integer qty;
+
+    public DeliveryAdded(Delivery aggregate) {
+        super(aggregate);
+    }
+
+    public DeliveryAdded() {
+        super();
+    }
+}
+//>>> DDD / Domain Event
